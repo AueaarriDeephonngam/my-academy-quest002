296222ddec0c7a1776c5bc4007192095ac793837
aueaarri.d <aueaarri.d@kkumail.com>
2025-08-18 00:50:05 +0700

    refactor quests controller and views for Turbo integration; add partials for quest count and list

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,8 +1,8 @@
 class QuestsController < ApplicationController
   before_action :set_quest, only: [ :destroy, :toggle ]
+  before_action :load_quests, only: [ :index, :create, :toggle, :destroy ]
 
   def index
-    @quests = Quest.order(created_at: :desc)
     @quest = Quest.new
   end
 
@@ -10,27 +10,42 @@ class QuestsController < ApplicationController
     @quest = Quest.new(quest_params)
 
     if @quest.save
-      redirect_to quests_path, notice: "Quest added successfully!"
+      respond_to do |format|
+        format.html { redirect_to quests_path, notice: "Quest added successfully!" }
+        format.turbo_stream
+      end
     else
-      @quests = Quest.order(created_at: :desc)
-      render :index
+      respond_to do |format|
+        format.html { render :index }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("quest-list", partial: "quest_list") }
+      end
     end
   end
 
   def toggle
     @quest.update(done: !@quest.done)
-    redirect_to quests_path
+    respond_to do |format|
+      format.html { redirect_to quests_path }
+      format.turbo_stream
+    end
   end
 
   def destroy
     @quest.destroy
-    redirect_to quests_path, notice: "Quest deleted successfully!"
+    respond_to do |format|
+      format.html { redirect_to quests_path, notice: "Quest deleted successfully!" }
+      format.turbo_stream
+    end
   end
 
   private
 
   def set_quest
     @quest = Quest.find(params[:id])
+  end
+
+  def load_quests
+    @quests = Quest.order(created_at: :desc)
   end
 
   def quest_params
