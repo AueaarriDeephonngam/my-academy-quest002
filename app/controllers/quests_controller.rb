1b0239f75016db553d512b280ec0f5c210603c31
aueaarri.d <aueaarri.d@kkumail.com>
2025-08-17 22:05:12 +0700

    creat quest index view

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,13 +1,39 @@
 class QuestsController < ApplicationController
+  before_action :set_quest, only: [ :destroy, :toggle ]
+
   def index
+    @quests = Quest.order(created_at: :desc)
+    @quest = Quest.new
   end
 
   def create
+    @quest = Quest.new(quest_params)
+
+    if @quest.save
+      redirect_to quests_path, notice: "Quest added successfully!"
+    else
+      @quests = Quest.order(created_at: :desc)
+      render :index
+    end
   end
 
   def toggle
+    @quest.update(done: !@quest.done)
+    redirect_to quests_path
   end
 
   def destroy
+    @quest.destroy
+    redirect_to quests_path, notice: "Quest deleted successfully!"
+  end
+
+  private
+
+  def set_quest
+    @quest = Quest.find(params[:id])
+  end
+
+  def quest_params
+    params.require(:quest).permit(:title)
   end
 end
